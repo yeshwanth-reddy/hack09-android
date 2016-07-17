aed57c214c97193be34c2889adf72c369379a4f0
Sasikumar <sasikumar@stayzilla.com>
2016-07-17 13:56:54 +0530

    Booking detail page UI completed

--- app/src/main/java/com/stayzilla/postbooking/MainActivity.java
+++ app/src/main/java/com/stayzilla/postbooking/MainActivity.java
@@ -3,12 +3,13 @@ package com.stayzilla.postbooking;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.google.gson.Gson;
+import com.squareup.picasso.Picasso;
 import com.stayzilla.postbooking.model.MyBooking;
 import com.stayzilla.postbooking.model.MyBookingsModel;
-import com.stayzilla.postbooking.model.PlacesModel;
-import com.stayzilla.postbooking.network.restclients.EventsAPIRestClient;
 import com.stayzilla.postbooking.network.restclients.MyBookingsAPIRestClient;
 
 import java.util.List;
@@ -22,9 +23,32 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_bookingdetail);
 
-        startActivity(new Intent(this, MapActivity.class));
+        ImageView imageView = (ImageView) findViewById(R.id.hotelImage);
+
+        Picasso.with(this)
+                .load("https://stay-imgs.stayzilla.com/resize/1200x800/89946/1124146.jpg")
+                .placeholder(R.drawable.ic_hotel_placeholder)
+                .error(R.drawable.ic_hotel_placeholder)
+                .into(imageView);
+
+        imageView = (ImageView) findViewById(R.id.locationImage);
+
+        Picasso.with(this)
+                .load("https://maps.googleapis.com/maps/api/staticmap?center=24.8366999,79.9213073&zoom=17&markers=color:pink%7Clabel:%7C24.8366999,79.9213073&size=800x400")
+                .placeholder(R.drawable.ic_hotel_placeholder)
+                .error(R.drawable.ic_hotel_placeholder)
+                .into(imageView);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, MapActivity.class));
+            }
+        });
+
+//        startActivity(new Intent(this, MapActivity.class));
 
 
 //        EventsAPIRestClient.getClient().getEvents("12","121").enqueue(new Callback<PlacesModel>() {
@@ -43,10 +67,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<MyBookingsModel> call, Response<MyBookingsModel> response) {
 
-                if(response.isSuccessful()) {
+                if (response.isSuccessful()) {
                     getBookings(response.body());
-                }
-                else{
+                } else {
                     getBookings(new MyBookingsModel());
                 }
 
@@ -61,12 +84,12 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void getBookings(MyBookingsModel bookingsResponse){
+    public void getBookings(MyBookingsModel bookingsResponse) {
         List<MyBooking> myBookings = bookingsResponse.myBooking;
-        for (int i=0; i<myBookings.size(); i++ ) {
+        for (int i = 0; i < myBookings.size(); i++) {
             MyBooking myBooking = myBookings.get(i);
             String response = new Gson().toJson(myBooking);
-            System.out.println("Logging My Booking="+response);
+            System.out.println("Logging My Booking=" + response);
             System.out.println(myBooking.stay.location.lat);
         }
     }
